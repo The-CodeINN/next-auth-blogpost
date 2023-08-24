060719053acc176473e07f55fed931191493bf02
The-CodeINN <emijere.richard@gmail.com>
2023-08-24 11:27:16 +0100

    Added sigin page

--- app/(landing)/signin/page.jsx
+++ app/(landing)/signin/page.jsx
@@ -1,7 +1,12 @@
+import Heading from '@/components/Heading';
 import React from 'react';
 
 const SiginPage = () => {
-  return <div>SiginPage</div>;
+  return (
+    <div className='flex items-center justify-center '>
+      <Heading heading='Sign In' description='Sign in to your account' />
+    </div>
+  );
 };
 
 export default SiginPage;
