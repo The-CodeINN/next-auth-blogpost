3389b17751ed5a7b889e6124ed01dca9cbbf5bcd
The-CodeINN <emijere.richard@gmail.com>
2023-08-24 11:26:29 +0100

    Worked on the layout

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,18 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Mastering Modern Authentication',
-  description: 'This is app tutorial on Mastering Modern Authentication: Building Secure User Flows with NextAuth, Prisma, MongoDB, and React Hook Form in Next.js',
-}
+  description:
+    'This is app tutorial on Mastering Modern Authentication: Building Secure User Flows with NextAuth, Prisma, MongoDB, and React Hook Form in Next.js',
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
