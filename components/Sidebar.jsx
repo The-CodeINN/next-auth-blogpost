8936dcd800c16cdef314d32aed7a80787b5f65fb
The-CodeINN <emijere.richard@gmail.com>
2023-08-24 11:29:46 +0100

    Made changes on the layout

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { LogIn, LogOut, X } from 'lucide-react';
+import { LogIn, LogOut, X, Bird } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -23,11 +23,11 @@ const Sidebar = ({ onClickX, onClickLink }) => {
     <div className='space-y-4 py-4 flex flex-col h-full text-white bg-[#111827]'>
       <div className='px-3 py-2 flex-1'>
         <div className='flex items-center justify-between mb-14 md:mb-0'>
-          <Link href='/' className='pl-3 md:flex md:items-center md:mb-14'>
+          <Link href='/' className='pl-3 flex items-center md:mb-14'>
             <div className='relative w-12 h-12 mr-4'>
-              <Image alt='Logo' fill src='/placeholder.jpg' />
+              <Bird className='absolute top-0 left-0 w-full h-full' />
             </div>
-            <h1 className='text-2xl font-bold hidden md:flex'>NextJS</h1>
+            <h1 className='text-2xl font-bold'>Auth</h1>
           </Link>
           <X
             className='h-8 w-8 text-gray-500 hover:text-gray-200 md:hidden cursor-pointer'
